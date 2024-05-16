4b17c7a786a816126f94aad58cf418d1f3551a46
ChomynNicolas <chomynnicolas2@gmail.com>
2024-05-16 14:30:48 -0400

    mas formularios tarea core

--- src/components/FormComp/FormComp.jsx
+++ src/components/FormComp/FormComp.jsx
@@ -30,16 +30,25 @@ const FormComp = () => {
     <>
     <form onSubmit={enviandoForm}>
       <div className="form-cont">
-        <label htmlFor="firstname">Firs Name:</label>
+        <label htmlFor="firstname">First Name:</label>
         <input type="text" id="firstname" value={user.firstName} onChange={(event)=>setUser({...user, firstName: event.target.value })}/>
+
+        {user.firstName.length?user.firstName.length<=2? <p style={{color: "red", margin: "0", textAlign: "center" }}>El nombre tiene que tener al menos 3 caracteres</p>:"" :"" }
+
       </div>
       <div className="form-cont">
         <label htmlFor="lastname">Last Name:</label>
         <input type="text"  id="lastname" value={user.lastName} onChange={(event)=>setUser({...user, lastName: event.target.value })}/>
+
+        {user.lastName.length?user.lastName.length<=2? <p style={{color: "red", margin: "0", textAlign: "center" }}>El apellido tiene que tener al menos 3 caracteres</p>:"" :"" }
+
       </div>
       <div className="form-cont">
         <label htmlFor="email">Email:</label>
         <input type="email" id="email" value={user.email} onChange={(event)=>setUser({...user, email: event.target.value })}/>
+
+        {user.email.length?user.email.length<=5? <p style={{color: "red", margin: "0", textAlign: "center" }}>El email tiene que tener al menos 6 caracteres</p>:"" :"" }
+
       </div>
       <div className="form-cont">
         <label htmlFor="password">Password:</label>
@@ -48,6 +57,11 @@ const FormComp = () => {
       <div className="form-cont">
         <label htmlFor="cpassword">Confirm Password:</label>
         <input type="password" id="cpassword" value={user.cpassword} onChange={(event)=>setUser({...user, cpassword: event.target.value })}/>
+        {(user.password.length||user.cpassword.length)?(user.password.length<=7||user.cpassword.length<=7)? <p style={{color: "red", margin: "0", textAlign: "center" }}>La contraseña debe tener al menos 8 caracteres</p>:"" :"" }
+        {user.password.length?!(user.password===user.cpassword)? <p style={{color: "red", margin: "0", textAlign: "center" }}>Las contraseñas debe coincidir</p>:"" :"" }
+
+
+
       </div>
       <button className="btn" type="submit">Send form</button>
     </form>
